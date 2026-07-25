7aa0e1f1e9578edd31a5b398944b0a7935b65d79
Abha <abhasingh0575@gmail.com>
2026-07-26 00:35:37 +0530

    Create planner app and configure app routing

---
+++ planner/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def all_planner(request):
+    return render(request, 'planner/all_planner.html')
+
--- studyplanner/urls.py
+++ studyplanner/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from . import views
 
 urlpatterns = [
@@ -25,4 +25,5 @@ urlpatterns = [
     path('subjects/', views.subjects, name='subjects'),
     path('task/', views.task, name='task'),
     path('contact/', views.contact, name='contact'),
+    path('planner/', include('planner.urls')),
 ]
